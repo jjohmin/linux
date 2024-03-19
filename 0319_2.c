20c76456f81ac2fb5f19c2c48e9a0373adb86498
jjohmin <jhmin7624@gmail.com>
2024-03-19 16:47:39 +0900

    20240319

--- 0319_2.c
+++ 0319_2.c
@@ -1,3 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+int main()
+{
+	int fd;
+	off_t nread;
+	char buffer[1024];
+	fd = open("alpha.txt",O_RDWR);
+	if(fd == -1){
+		printf("Error!");
+		exit(1);
+	}
+
+	while((nread=read(fd,buffer,sizeof(buffer)))>0){
+		for(int cnt = 0;cnt<nread;cnt++){
+			if(buffer[cnt]>='a' && buffer[cnt]<='z'){
+				buffer[cnt]=buffer[cnt]-'a'+'A';
+			}
+			lseek(fd,(off_t)-nread,SEEK_CUR);
+			write(fd,buffer,nread);
+		}
+	}
+	close(fd);
+}
