e70832c81fbc277727f031ccdfdd232629ff74c5
jjohmin <jhmin7624@gmail.com>
2024-04-10 18:25:55 +0900

    20240409

--- 2024-04-09/0409_4.c
+++ 2024-04-09/0409_4.c
@@ -6,16 +6,13 @@
 int main(int argc,char* argv[]){
 	pid_t pid;
 
-	for(int i=0;i<argc;i++)
+	for(int i=1;i<argc;i++){
 		pid = fork();
-
-	if(pid > 0){
-		execl("/bin/wc","wc","-l","-w","-c",(char*)0);
+		if(pid == 0){
+			execl("/bin/wc","wc","-l","-w","-c",argv[i],(char*)0);
+		}
+		else if(pid>0){
+			sleep(1);
+		}
 	}
-	else if(pid == 0){
-		execl("/bin/wc","wc","-l","-w","-c",(char*)0);
-	}
-	else
-		printf("fail\n");
-
 }
